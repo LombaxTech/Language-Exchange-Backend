713b3d33f84986368efd040c05f699d332f5780d
Rakib Khan <rakibkhan@live.co.uk>
2020-09-01 04:06:24 +0100

    .

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const createPostModel = require("../models/posts.model");
+const createChatModel = require("../models/chat.model");
 
 module.exports = function (app) {
   app.get("/custom-posts", async (req, res) => {
@@ -9,4 +10,15 @@ module.exports = function (app) {
       .populate("user", "-password -createdAt");
     res.json(posts);
   });
+
+  app.get("/custom-posts/:userId", async (req, res) => {
+    const Post = createPostModel(app);
+
+    const { userId } = req.params;
+
+    let posts = await Post.find({ user: userId }).populate("user", "-password");
+    res.json(posts);
+  });
+
+  // app.post("/send-message", )
 };
